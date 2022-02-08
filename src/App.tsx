7e59b6e58c63e2b94babc241b2790d45ec1f080e
Panaite Cristian-Florin <panaite.cristian12@gmail.com>
2022-02-09 00:41:10 +0200

    haida

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className='navbar'>
+        <ul>
+          <li>
+            <span>About</span>
+          </li>
+          <li>
+            <span>Experience</span>
+          </li>
+          <li>
+            <span>Work</span>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </div>
+
+    <div className='container'>
+      <div className='sidebar'>
+        <ul>
+          <li>
+            <span><FontAwesomeIcon icon={faGithub} /></span>
+          </li>
+          <li>
+            <span><FontAwesomeIcon icon={faYoutube} /></span>
+          </li>
+          <li>
+            <span><FontAwesomeIcon icon={faFacebook} /></span>
+          </li>
+          <li>
+            <span><FontAwesomeIcon icon={faInstagram}/></span>
+          </li>
+        </ul>
+      </div>
+
+    <div className='heading'>
+      <h5>
+        Hi, my name is
+      </h5>
+
+      <h2>
+        Panaite Cristian.
+      </h2>
+
+      <h3>
+        I do stuff on the web.
+      </h3>
+
+      <p>
+        I'm a web developer. Simplu si pe 2.
+      </p>
+
+      <a href="youtube.com">
+        Check out my tutorials !
+      </a>
+    </div>
+    </div>
     </div>
   );
 }
