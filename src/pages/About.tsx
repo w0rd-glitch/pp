f68d5bd3ad54e7fcfc5544ce1fc37513fdabfbcf
Panaite Cristian-Florin <panaite.cristian12@gmail.com>
2022-02-10 21:07:34 +0200

    putin la about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,20 +3,22 @@ import React from 'react'
 function About() {
   return (
     <div className='about-page'>
-        <div className='about-container'>
-          <div className='about-text'>
+        <div className='about-heading'>
+          <h2>About me</h2>
+          <hr></hr>
+        </div>
+        <div className='about-text-img'>
+          <div>
+            <p>Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking together a custom reblog button taught me a lot about HTML & CSS!
 
-            <div className='about-text-heading'>
-            <h4>About Me</h4>
-            <hr></hr>
-            </div>
+Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.
 
-            <div className='about-text-paragraph'>
-                <p>Hello everybody, i want to kill myself</p>
-            </div>
+I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+
+Here are a few technologies I’ve been working with recently:</p>
           </div>
-          <div className='about-img'>
-
+          <div>
+            <img src="/cristi.jpeg" alt="cristi" />
           </div>
         </div>
     </div>
