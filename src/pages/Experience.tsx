d638476c0a62e458f6409e6d23c66e6522f76eae
Panaite Cristian-Florin <panaite.cristian12@gmail.com>
2022-03-09 10:41:03 +0200

    cleared experience page

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -3,14 +3,6 @@ import React from 'react'
 function Experience() {
   return (
     <div className='experience-page'>
-        <div className='experience-heading'>
-            <h3>Where I've worked</h3>
-            <hr></hr>
-        </div>
-        <div className='experience-list'>
-            <h3>something here just for</h3>
-            <h3>something here just for</h3>
-        </div>
     </div>
   )
 }
